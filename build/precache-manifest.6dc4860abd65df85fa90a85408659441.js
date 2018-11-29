cf792c0820e2a8d988ac0a26a19f3cb7fe4f23d9
manojkumarg16 <manojganesan216@gmail.com>
2018-11-30 00:06:30 +0530

    remove head

---
+++ build/precache-manifest.6dc4860abd65df85fa90a85408659441.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aebf39822e42541a514c",
+    "url": "/static/css/main.8c96535b.chunk.css"
+  },
+  {
+    "revision": "aebf39822e42541a514c",
+    "url": "/static/js/main.aebf3982.chunk.js"
+  },
+  {
+    "revision": "79e266424590225dd789",
+    "url": "/static/js/1.79e26642.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d85f94c04e40e76519be8cb048105bb7",
+    "url": "/index.html"
+  }
+];
