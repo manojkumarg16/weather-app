795de466c2708f3531e62f119eb45e5c5f5dc8f3
manojkumarg16 <manojganesan216@gmail.com>
2018-11-29 23:52:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const request = require("request");
+const path = require("path");
+const port = 3002;
+
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); //My frontend APP domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.get("/getWeather", async (req, res) => {
+  let { city, appid } = req.query;
+  let url = `https://samples.openweathermap.org/data/2.5/forecast?q=${city}&appid=${appid}`;
+  console.log(city, appid, url, req.query)
+  request.get(
+    {
+      url: url
+    },
+    function(error, response, body) {
+      res.json(JSON.parse(response.body));
+    }
+  );
+});
+app.listen(port, () => {
+  console.log(`Server listening on the port ${port}`);
+});
---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import request from "request";
+import moment from "moment";
+import "./dashboard.css";
+const { REACT_APP_API_KEY } = process.env;
+
+export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      city: "london",
+      weather: [],
+      cities: [
+        { city: "London", code: "london" },
+        { city: "London", code: "london" },
+        { city: "London", code: "london" },
+        { city: "London", code: "london" },
+        { city: "London", code: "london" },
+        { city: "London", code: "london" }
+      ]
+    };
+  }
+  getWeather = city => {
+    this.setState({ isLoading: true });
+    request.get(
+      {
+        url: `http://localhost:3002/getWeather?city=${city}&appid=${REACT_APP_API_KEY}`
+      },
+      (error, response, body) => {
+        console.log(JSON.parse(body));
+        this.setState({ isLoading: false, weather: JSON.parse(body).list });
+      }
+    );
+  };
+  showWeatherCard = (detail, index) => {
+    return (
+      <div className="card" key={index}>
+        <img
+          className="card-img-top"
+          src={`http://openweathermap.org/img/w/${detail.weather[0].icon}.png`}
+          alt="Weather status"
+        />
+        <div className="card-body">
+          <h5 className="card-title">
+            {moment(detail.dt_txt, "YYYY-MM-DD hh:mm:ss").format("MMM Do YYYY")}
+          </h5>
+          <p className="card-text">{detail.weather[0].description}</p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Pressure:</b> {detail.main.pressure}
+          </li>
+          <li className="list-group-item">
+            <b>Temprature:</b> {detail.main.temp}
+          </li>
+          <li className="list-group-item">
+            <b>Humidity:</b> {detail.main.humidity}
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  onChange = e => {
+    this.setState({ city: e.target.value });
+  };
+  render() {
+    const { weather, city, isLoading } = this.state;
+    return (
+      <div>
+        <div className="content">
+          <h3>Weather Forecast</h3>
+          <div className="section" id="section">
+            <div className="row" id="row">
+              <input
+                type="text"
+                name="city"
+                className="class"
+                placeholder="Search here.."
+                id="word"
+                onChange={this.onChange}
+              />
+              <button
+                className="btn btn-primary btn-lg"
+                onClick={() => this.getWeather(city)}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+          {!isLoading ? (
+            weather.length ? (
+              weather.map((data, index) => {
+                return this.showWeatherCard(data, index);
+              })
+            ) : (
+              <h1>No weather</h1>
+            )
+          ) : (
+            <h1>Loading</h1>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
